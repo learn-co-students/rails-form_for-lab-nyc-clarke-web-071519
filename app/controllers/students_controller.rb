b12883d08f13c8b9dbe782b2d0255fd2d6f5c176
AJ <amarchitelli1993@gmail.com>
2019-08-11 16:45:57 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController 
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:index, :new, :update, :create, :edit, :show]
+
+  resources :school_classes, only: [:index, :new, :update, :create, :edit, :show]
 end
